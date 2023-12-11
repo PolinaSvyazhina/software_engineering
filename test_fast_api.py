ea9c88158115f77076ab3cd0bb89070a1c3f21c7
Polina Svyazhina <kiramik55@gmail.com>
2023-12-11 20:49:55 +0500

    added practice 5

---
+++ test_fast_api.py
@@ -0,0 +1,15 @@
+from fastapi.testclient import TestClient
+from fastApiPractice import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    responses = client.get("/")
+    assert responses.status_code == 200
+    assert responses.json() == {'message': 'Hello Word, this is image-classification model'}
+
+
+def test_predict():
+    responses = client.post('/predict/')
+    assert responses.status_code == 200
